c515bd8afa87b7b78f36c0821d71bafd1923919e
bryant273092 <bryanth1@uci.edu>
2020-11-23 19:48:58 -0800

    temp order page

--- pages/order.js
+++ pages/order.js
@@ -1,11 +1,13 @@
 import PageHeading from '../components/pageHeading'
 import HOC from '../components/hoc';
+import {HeaderOne} from '../components/layout'
 
 const WrappedComponents = () => {
   
     return (
       <>
         <PageHeading title="Order Online"></PageHeading>
+        <HeaderOne>Coming Soon...</HeaderOne>
       </>
     )
   }
